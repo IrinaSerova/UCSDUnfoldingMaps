e248feb0539136bb98d1d2095dad5c5414fa38ca
Irina Serova <isserova@gmail.com>
2018-05-22 15:58:29 -0700

    changed demo Student.java

--- src/demos/Person.java
+++ src/demos/Person.java
@@ -8,28 +8,43 @@ package demos;
 public class Person 
 {
 	private String name; 
-	
+
 	public Person(String name)  
 	{ 
 		this.name = name; 
 	}
-	
+
 	public boolean isAsleep(int hr)  
 	{ 
 		return 22 < hr || 7 > hr; 
 	}
-	  
+	public boolean Party(String day)  
+	{ 
+		return "Sun" == day || "Sat" == day; 
+	}
+
+
 	public String toString()      
 	{ 
 		return name; 
 	}
 
-	public void status( int hr )
+	public void status( int hr)
 	{
 		if ( this.isAsleep( hr ) )
 			System.out.println( "Now offline: " + this );
-	    else
-	    	System.out.println( "Now online: " + this + ". Time is: " + hr + " : 00");
-	 }
-	
+		else
+			System.out.println( "Now online: " + this + ". Time is: " + hr + " : 00");		
+
+	}
+	public void dayParty( String day)
+	{
+		if ( this.Party( day ) )
+			System.out.println( "Good Day for Party: " + day );
+		else
+			System.out.println( "Time for Study: " + day);		
+
+	}
+
+
 }
