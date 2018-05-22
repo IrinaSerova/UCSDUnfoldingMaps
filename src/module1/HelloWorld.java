0eeaa3ecfc78b547d2bce0f35f2d1270fba238cb
Irina Serova <isserova@gmail.com>
2018-05-22 16:49:42 -0700

    changed module1

--- src/module1/HelloWorld.java
+++ src/module1/HelloWorld.java
@@ -27,7 +27,7 @@ public class HelloWorld extends PApplet
 	public static String mbTilesString = "blankLight-1-3.mbtiles";
 	
 	// IF YOU ARE WORKING OFFLINE: Change the value of this variable to true
-	private static final boolean offline = false;
+	private static final boolean offline = true;
 	
 	/** The map we use to display our home town: La Jolla, CA */
 	UnfoldingMap map1;
@@ -65,7 +65,7 @@ public class HelloWorld extends PApplet
 		// The 6th argument specifies the map provider.  
 		// There are several providers built-in.
 		// Note if you are working offline you must use the MBTilesMapProvider
-		map1 = new UnfoldingMap(this, 30, 50, 350, 500, provider);
+		map1 = new UnfoldingMap(this, 20, 50, 350, 500, provider);
 		map2 = new UnfoldingMap(this, 430, 50, 350, 500, provider);
 
 		// The next line zooms in and centers the map at 
